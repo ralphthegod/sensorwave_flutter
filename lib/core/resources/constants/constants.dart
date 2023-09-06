abab1715f84b97714e29a88a60336fc4d6875ec3
RalphTheCreator <rafdimaso@gmail.com>
2023-09-06 15:56:12 +0200

    feat: add keycloak api url

--- lib/core/resources/constants/constants.dart
+++ lib/core/resources/constants/constants.dart
@@ -1,1 +1,11 @@
 const String apiBaseUrl = '';
+
+// Keycloak
+const String keycloakSourceUrl = 'http://{{keycloak-host}}';
+const String keycloakRealm = 'master';
+const String keycloakApiBaseUrl = '$keycloakSourceUrl/realms/$keycloakRealm/protocol/openid-connect';
+
+// Client
+const String tokenClientId = '';
+const String tokenClientSecret = '';
+const String tokenClientGrantType = 'password';
