4b21c3abad42cbbe81955564edfddd74ef60632a
RalphTheCreator <rafdimaso@gmail.com>
2023-09-06 16:00:57 +0200

    feat: add AuthRepositoryImpl class

---
+++ lib/features/iot-security/data/repository/auth_repository_impl.dart
@@ -0,0 +1,69 @@
+
+import 'dart:io';
+
+import 'package:dio/dio.dart';
+import 'package:sensorwave/core/resources/constants/constants.dart';
+import 'package:sensorwave/core/resources/data_state.dart';
+import 'package:sensorwave/features/iot-security/data/data_sources/auth_api_service.dart';
+import 'package:sensorwave/features/iot-security/data/data_sources/token_api_service.dart';
+import 'package:sensorwave/features/iot-security/data/models/client_access_token/client_access_token.dart';
+import 'package:sensorwave/features/iot-security/data/models/user/user.dart';
+import 'package:sensorwave/features/iot-security/domain/repository/auth_repository.dart';
+
+class AuthRepositoryImpl extends AuthRepository {
+
+  final AuthApiService _authApiService;
+  final TokenApiService _tokenApiService;
+  AuthRepositoryImpl(this._authApiService, this._tokenApiService);
+
+  @override
+  Future<DataState<ClientAccessToken>> login(String username, String password) async {
+    try {
+      final httpResponse = await _tokenApiService.getAccessToken(
+        username,
+        password,
+        tokenClientGrantType,
+        tokenClientSecret,
+        tokenClientId
+      );
+      
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        return DataFailed(
+          DioException(
+            error: httpResponse.response.statusMessage,
+            response: httpResponse.response,
+            type: DioExceptionType.badResponse,
+            requestOptions: httpResponse.response.requestOptions
+          )
+        );
+      }
+    } on DioException catch(e){
+      return DataFailed(e);
+    }
+  }
+
+  @override
+  Future<DataState<User>> register(String username, String password) async {
+    try {
+      final httpResponse = await _authApiService.register(username, password);
+      
+      if (httpResponse.response.statusCode == HttpStatus.ok) {
+        return DataSuccess(httpResponse.data);
+      } else {
+        return DataFailed(
+          DioException(
+            error: httpResponse.response.statusMessage,
+            response: httpResponse.response,
+            type: DioExceptionType.badResponse,
+            requestOptions: httpResponse.response.requestOptions
+          )
+        );
+      }
+    } on DioException catch(e){
+      return DataFailed(e);
+    }
+  }
+
+}
