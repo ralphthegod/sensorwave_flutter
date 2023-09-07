abfe9128477ec8f20f5a106fe422f948e73a6ed0
RalphTheCreator <rafdimaso@gmail.com>
2023-09-07 16:38:44 +0200

    feat: add RoomApiService abstract class (and gen.)

---
+++ lib/features/iot-processor/data/data_sources/room_api_service.dart
@@ -0,0 +1,27 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:sensorwave/core/resources/constants/constants.dart';
+import 'package:dio/dio.dart' as dio;
+import 'package:sensorwave/features/iot-processor/data/models/room/room.dart';
+
+part 'room_api_service.g.dart';
+
+@RestApi(baseUrl: apiBaseUrl)
+abstract class RoomApiService{
+  factory RoomApiService(dio.Dio dio, {String baseUrl}) = _RoomApiService;
+
+  @POST("/rooms/createRoom")
+  Future<HttpResponse<Room>> createRoom(
+    @Header("Authorization") String accessToken,
+    @Query("name") String name,
+    @Query("roomOwnerUsername") String roomOwnerUsername,
+  );
+
+  @GET("/rooms/{roomOwnerUsername}")
+  Future<HttpResponse<List<Room>>> getRoomsByOwnerUsername(
+    @Header("Authorization") String accessToken,
+    @Path("roomOwnerUsername") @Query("roomOwnerUsername") String roomOwnerUsername,
+  );
+
+  
+}
---
+++ lib/features/iot-processor/data/data_sources/room_api_service.g.dart
@@ -0,0 +1,122 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'room_api_service.dart';
+
+// **************************************************************************
+// RetrofitGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_brace_in_string_interps,no_leading_underscores_for_local_identifiers
+
+class _RoomApiService implements RoomApiService {
+  _RoomApiService(
+    this._dio, {
+    this.baseUrl,
+  });
+
+  final Dio _dio;
+
+  String? baseUrl;
+
+  @override
+  Future<HttpResponse<Room>> createRoom(
+    String accessToken,
+    String name,
+    String roomOwnerUsername,
+  ) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{
+      r'name': name,
+      r'roomOwnerUsername': roomOwnerUsername,
+    };
+    final _headers = <String, dynamic>{r'Authorization': accessToken};
+    _headers.removeWhere((k, v) => v == null);
+    final Map<String, dynamic>? _data = null;
+    final _result = await _dio
+        .fetch<Map<String, dynamic>>(_setStreamType<HttpResponse<Room>>(Options(
+      method: 'POST',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/rooms/createRoom',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    final value = Room.fromJson(_result.data!);
+    final httpResponse = HttpResponse(value, _result);
+    return httpResponse;
+  }
+
+  @override
+  Future<HttpResponse<List<Room>>> getRoomsByOwnerUsername(
+    String accessToken,
+    String roomOwnerUsername,
+  ) async {
+    const _extra = <String, dynamic>{};
+    final queryParameters = <String, dynamic>{
+      r'roomOwnerUsername': roomOwnerUsername
+    };
+    final _headers = <String, dynamic>{r'Authorization': accessToken};
+    _headers.removeWhere((k, v) => v == null);
+    final Map<String, dynamic>? _data = null;
+    final _result = await _dio
+        .fetch<List<dynamic>>(_setStreamType<HttpResponse<List<Room>>>(Options(
+      method: 'GET',
+      headers: _headers,
+      extra: _extra,
+    )
+            .compose(
+              _dio.options,
+              '/rooms/${roomOwnerUsername}',
+              queryParameters: queryParameters,
+              data: _data,
+            )
+            .copyWith(
+                baseUrl: _combineBaseUrls(
+              _dio.options.baseUrl,
+              baseUrl,
+            ))));
+    var value = _result.data!
+        .map((dynamic i) => Room.fromJson(i as Map<String, dynamic>))
+        .toList();
+    final httpResponse = HttpResponse(value, _result);
+    return httpResponse;
+  }
+
+  RequestOptions _setStreamType<T>(RequestOptions requestOptions) {
+    if (T != dynamic &&
+        !(requestOptions.responseType == ResponseType.bytes ||
+            requestOptions.responseType == ResponseType.stream)) {
+      if (T == String) {
+        requestOptions.responseType = ResponseType.plain;
+      } else {
+        requestOptions.responseType = ResponseType.json;
+      }
+    }
+    return requestOptions;
+  }
+
+  String _combineBaseUrls(
+    String dioBaseUrl,
+    String? baseUrl,
+  ) {
+    if (baseUrl == null || baseUrl.trim().isEmpty) {
+      return dioBaseUrl;
+    }
+
+    final url = Uri.parse(baseUrl);
+
+    if (url.isAbsolute) {
+      return url.toString();
+    }
+
+    return Uri.parse(dioBaseUrl).resolveUri(url).toString();
+  }
+}
