a86ef1cb6cc964b7d2216948315303233295ab1c
RalphTheCreator <rafdimaso@gmail.com>
2023-09-07 16:39:40 +0200

    feat: add UserData class

---
+++ lib/core/resources/user_data.dart
@@ -0,0 +1,8 @@
+import 'package:sensorwave/features/iot-security/data/models/client_access_token/client_access_token.dart';
+import 'package:sensorwave/features/iot-security/data/models/user/user.dart';
+
+
+class UserData {
+  static late User user;
+  static late ClientAccessToken clientAccessToken;
+}
