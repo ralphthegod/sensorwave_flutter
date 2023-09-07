96ccda7a3a6870544deac48efd454e5d34da198d
RalphTheCreator <rafdimaso@gmail.com>
2023-09-07 19:26:32 +0200

    feat: add UseCaseObserver notification in LoginUseCase class

--- lib/core/usecase/usecase.dart
+++ lib/core/usecase/usecase.dart
@@ -1,3 +1,15 @@
+import 'package:sensorwave/core/event/observer.dart';
+
 abstract class UseCase<Type,Params> {
+  List<UseCaseObserver> observers = [];
+
   Future<Type> call({required Params params});
+
+  void notifyObservers(dynamic data){
+
+    for (var observer in observers) {
+      observer.onUseCaseSuccess(data);
+    }
+  }
+  
 }
