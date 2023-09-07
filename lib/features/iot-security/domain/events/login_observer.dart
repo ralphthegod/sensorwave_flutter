8d81c6429487cac34c9e1de675a7d15c217bfa89
RalphTheCreator <rafdimaso@gmail.com>
2023-09-07 19:27:16 +0200

    feat: add LoginObserver class as LoginUseCase observer

---
+++ lib/features/iot-security/domain/events/login_observer.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: slash_for_doc_comments
+
+import 'package:sensorwave/core/event/observer.dart';
+import 'package:sensorwave/core/resources/user_data.dart';
+import 'package:sensorwave/features/iot-security/domain/models/client_access_token/client_access_token.dart';
+
+/**
+ * Observer for the login use case.
+ * 
+ * This observer is responsible for storing the client access token in the UserData class.
+ */
+
+class LoginObserver extends UseCaseObserver {
+  @override
+  void onUseCaseSuccess(dynamic useCaseOutput) {
+    ClientAccessToken clientAccessToken = useCaseOutput;
+    UserData.clientAccessToken = clientAccessToken;
+  }
+}
