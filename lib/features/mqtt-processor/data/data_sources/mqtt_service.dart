f2b467f9d6a685b2383d4d0453d7c21179f33359
RalphTheCreator <rafdimaso@gmail.com>
2023-09-12 01:03:31 +0200

    feat: add MqttService class

---
+++ lib/features/mqtt-processor/data/data_sources/mqtt_service.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/widgets.dart';
+import 'package:mqtt_client/mqtt_client.dart';
+import 'package:mqtt_client/mqtt_server_client.dart';
+import 'package:sensorwave/core/resources/constants/constants.dart';
+import 'package:sensorwave/core/resources/proto/output/iot-processor.pb.dart';
+import 'package:sensorwave/core/resources/user_data.dart';
+import 'package:sensorwave/core/util/mqtt_processor.dart';
+
+class MqttService{
+  
+  late MqttClient client;
+  late Function(SmartObjectMessage) streamCallback;
+  final MqttMessageProcessor _mqttMessageProcessor = MqttMessageProcessor();
+  
+  Future<MqttConnectionState?> connect(String clientIdentifier, String topic, Function(SmartObjectMessage) streamCallback) async{
+    client = MqttServerClient.withPort(
+      mqttServerAddress,
+      clientIdentifier,
+      mqttServerPort,
+    );
+
+    client.autoReconnect = true;
+
+    client.onConnected = onConnected;
+    client.onDisconnected = onDisconnected;
+    this.streamCallback = streamCallback;
+
+    MqttClientConnectionStatus? mqttConnectionStatus =
+        await client.connect(UserData.user.username, UserData.user.password).catchError(
+      (error) {
+        return null;
+      },
+    );
+
+     if (mqttConnectionStatus?.state == MqttConnectionState.connected) {
+       client.subscribe(topic, MqttQos.atLeastOnce);
+     }
+    
+
+    return mqttConnectionStatus?.state;
+  }
+
+  void disconnect(){
+    client.disconnect();
+  }
+
+  void onConnected() {
+    logger.d("MQTT Connected");
+    client.updates?.listen(
+      (List<MqttReceivedMessage<MqttMessage>> messages) {
+
+        for (MqttReceivedMessage mqttReceivedMessage in messages) {
+
+          final topic = mqttReceivedMessage.topic;
+
+          final payload = mqttReceivedMessage.payload as MqttPublishMessage;
+          final message = _mqttMessageProcessor.processBuffer(payload.payload.message.buffer);
+  
+          logger.d('received topic: "$topic", message<dynamic>: "$message"');
+          _mqttMessageProcessor.logData(message);
+
+          streamCallback(message);
+        }
+      },
+    );
+  }
+
+  void onDisconnected(){
+    logger.d("MQTT Disconnected");
+  }
+}
---
+++ lib/features/mqtt-processor/data/mqtt_service_provider.dart
@@ -0,0 +1,19 @@
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sensorwave/core/resources/proto/output/iot-processor.pb.dart';
+import 'package:sensorwave/features/mqtt-processor/data/data_sources/mqtt_service.dart';
+import 'package:sensorwave/features/mqtt-processor/data/repository/mqtt_reactive_repository_impl.dart';
+import 'package:sensorwave/features/mqtt-processor/domain/repository/mqtt_reactive_repository.dart';
+
+final mqttRepositoryStreamProvider = StreamProvider<List<SmartObjectMessage>>((ref) async* { 
+  final mqttService = MqttService();
+  final mqttRepository = MqttReactiveRepositoryImpl(mqttService);
+  await mqttRepository.connect("clientIdentifier", "topic");
+
+  ref.onDispose(() { 
+    mqttRepository.disconnect();});
+
+  await for (final message in mqttRepository.stream) {
+    yield [message];
+  }
+});
