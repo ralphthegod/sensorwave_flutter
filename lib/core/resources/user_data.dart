b9664912c8fda38f79fc0d3291b6e90cde3175b5
RalphTheCreator <rafdimaso@gmail.com>
2023-09-12 18:11:35 +0200

    feat: add tokenForApi in UserData class

--- lib/core/resources/user_data.dart
+++ lib/core/resources/user_data.dart
@@ -3,6 +3,7 @@ import 'package:sensorwave/features/iot-security/domain/models/user/user.dart';
 
 
 class UserData {
-  static late User user;
-  static late ClientAccessToken clientAccessToken;
+  static User user = const User();
+  static ClientAccessToken clientAccessToken = const ClientAccessToken(access_token: "", expires_in: 0, refresh_expires_in: 0, refresh_token: "", token_type: "", not_before_policy: 0, session_state: "", scope: "");
+  static String tokenForApi = "";
 }
