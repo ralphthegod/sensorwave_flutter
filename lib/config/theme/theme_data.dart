ce6a91af24ddfd951a24b7fbd9a06051ff757ef1
RalphTheCreator <rafdimaso@gmail.com>
2023-09-14 19:16:33 +0200

    feat: configure light ThemeData

---
+++ lib/config/theme/theme_data.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme = ThemeData(
+  brightness: Brightness.light,
+  useMaterial3: true,
+  textTheme: const TextTheme(
+    displayLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+    bodyLarge: TextStyle(fontSize: 18, color: Colors.black87),
+  ),
+  appBarTheme: const AppBarTheme(
+    color: Colors.blue,
+    iconTheme: IconThemeData(color: Colors.white),
+  ),
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+);
