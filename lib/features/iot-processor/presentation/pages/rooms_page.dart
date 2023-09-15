a1407a89ad856a2420b9e149e4f9740ecb780c85
RalphTheCreator <rafdimaso@gmail.com>
2023-09-16 01:37:12 +0200

    feat: add Rooms page

--- lib/features/iot-processor/presentation/pages/rooms_page.dart
+++ lib/features/iot-processor/presentation/pages/rooms_page.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sensorwave/core/resources/constants/constants.dart';
 import 'package:sensorwave/core/resources/data_state.dart';
 import 'package:sensorwave/features/iot-processor/domain/models/room/room.dart';
+import 'package:sensorwave/features/iot-processor/presentation/pages/create_room_page.dart';
+import 'package:sensorwave/features/iot-processor/presentation/pages/smart_objects_page.dart';
 import 'package:sensorwave/features/iot-processor/presentation/states/get_rooms_state_provider.dart';
+import 'package:sensorwave/features/iot-processor/presentation/widgets/room_item.dart';
 import 'package:sensorwave/features/shared/presentation/widgets/app_bar.dart';
 
 // ignore: must_be_immutable
@@ -20,39 +24,63 @@ class RoomsPage extends ConsumerWidget{
     this.context = context;
     state = ref.watch(getRoomsStateProvider);
     return Scaffold(
-      appBar: const MainAppBar(automaticallyImplyLeading: false),
-      body:  Center(
-        child: SingleChildScrollView(
-          child:
-          Consumer(builder:(context, ref, child) {
-            ref.read(getRoomsStateProvider.notifier).fetch({});
-            if (state is DataLoading) {
-              return const CircularProgressIndicator();
-            } else if(state is DataEmpty ) {
-              return const Text("No rooms found.");
-            }else if(state is DataSuccess<List<Room>>){
-              return _getRoomsListView();
-            }
-            else if(state is DataFailed){
-              return const Text("Error in fetching rooms.");
-            }
-            else{
-              return const Text("Unknown error.");
-            }
-          },)
-        )
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          final result = await Navigator.push(context, MaterialPageRoute(builder: (context) => CreateRoomPage()));
+          if(result is DataSuccess) ref.read(getRoomsStateProvider.notifier).fetch({});
+        },
+        child: const Icon(Icons.add),
       ),
-    );
+      appBar: const MainAppBar(automaticallyImplyLeading: false,),
+      body:
+          SingleChildScrollView(
+          child: Column(
+            mainAxisSize:MainAxisSize.min,
+            children: [
+            
+            const SizedBox(height: 30,),
+
+            const Text(
+                  'Your rooms',
+                  style: TextStyle(fontSize: 32),  
+                ),
+              
+            const SizedBox(height: 30,),
+
+            Consumer(builder:(context, ref, child) {
+              if (state is DataLoading) {
+                return const CircularProgressIndicator();
+              } else if(state is DataEmpty ) {
+                return const Text("No rooms found.");
+              }else if(state is DataSuccess<List<Room>>){
+                return _getRoomsListView();
+              }
+              else if(state is DataFailed){
+                return const Text("Error in fetching rooms.");
+              }
+              else{
+                return const Text("Unknown error.");
+              }
+            },)
+          ],)
+          
+        ),
+      );
+  }
+
+  void _openRoomCallback(Room room) async {
+    final result = await Navigator.of(context).push(MaterialPageRoute(builder: (context) => SmartObjectsPage(room: room)));
+    ref.read(getRoomsStateProvider.notifier).fetch({});
   }
 
   Widget _getRoomsListView() {
     return ListView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      scrollDirection: Axis.vertical,
+      shrinkWrap: true,
       itemCount: state.data!.length,
       itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(state.data![index].name),
-          subtitle: Text(state.data![index].id),
-        );
+        return RoomListItem(room: state.data![index], openRoomCallback: _openRoomCallback);
       },
     );
   }
