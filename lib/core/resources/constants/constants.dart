836d4caa8430904b6c275cd2774fbfe3d55f7b37
RalphTheCreator <rafdimaso@gmail.com>
2023-09-16 08:49:28 +0200

    feat: add required constants

--- lib/core/resources/constants/constants.dart
+++ lib/core/resources/constants/constants.dart
@@ -1,11 +1,26 @@
+
+import 'package:logger/logger.dart';
+
 const String apiBaseUrl = '';
+const String contentTypeFormUrlEncoded = 'application/x-www-form-urlencoded';
+const String contentTypeJson = 'application/json';
 
 // Keycloak
-const String keycloakSourceUrl = 'http://{{keycloak-host}}';
-const String keycloakRealm = 'master';
+const String keycloakSourceUrl = apiBaseUrl;
+const String keycloakRealm = 'app';
 const String keycloakApiBaseUrl = '$keycloakSourceUrl/realms/$keycloakRealm/protocol/openid-connect';
 
 // Client
-const String tokenClientId = '';
+const String tokenClientId = 'ext-client';
 const String tokenClientSecret = '';
 const String tokenClientGrantType = 'password';
+
+// Debug
+const bool isProduction = false;
+
+// MQTT
+const String mqttServerAddress = '';
+const int mqttServerPort = 18053;
+
+// Logs
+final logger = Logger();
